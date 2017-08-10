f01c82697895c8e8ab3c685828d9d72ca4cd4a6c
acioliwilson <30907754+acioliwilson@users.noreply.github.com>
2017-08-10 15:00:49 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <title>Allioop - Your clothes virtual store</title>
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+    <h1>Hello world!</h1>
+  </body>
+</html>
